45a6fbd953c28a14fe70c70147ccb853755667ac
Martin Vichnal <vichnalmartin@gmail.com>
2023-12-31 19:09:16 +0100

    v1.1.2
    Minor fixes

--- src/AntiDelay.h
+++ src/AntiDelay.h
@@ -3,7 +3,7 @@
  * @author Martin Vichnál
  * @page https://github.com/martinvichnal/AntiDelay
  * @brief AntiDelay is a library that aims to provide non-blocking delay functionality.
- * @version v1.1.0
+ * @version v1.1.2
  * @date 2023-12-31
  *
  * @copyright Copyright (c) 2024
